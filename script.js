f3aaacb7db1bf98ea12597ae5839c0cda7127da6
shrutiPatil <155883652+PatilShrutii@users.noreply.github.com>
2024-02-28 16:21:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// // Initialize and display the map
+// function initMap() {
+//     // Specify the coordinates to center the map
+//     var myLatLng = {lat: -34.397, lng: 150.644};
+  
+//     // Create a new map instance
+//     var map = new google.maps.Map(document.getElementById('map'), {
+//       center: myLatLng,
+//       zoom: 8 // Adjust zoom level as needed
+//     });
+  
+//     // Optionally, add markers or other features to the map
+//     var marker = new google.maps.Marker({
+//       position: myLatLng,
+//       map: map,
+//       title: 'Hello World!'
+//     });
+//   }
+  
